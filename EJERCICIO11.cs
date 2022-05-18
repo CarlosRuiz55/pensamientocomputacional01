c63eb9617bf71cd4f73e64fcd9dc076007d0631d
carlosruiz55 <04carlosruiz@gmail.com>
2022-05-18 13:34:14 -0600

    primer commit

---
+++ EJERCICIO11.cs
@@ -0,0 +1,28 @@
+﻿+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vluz;
+            vluz = 300000;
+            double distancia;
+            double tiempo;
+
+            Console.WriteLine("Introduzca el tiempo en segundos: ");
+            tiempo = double.Parse(Console.ReadLine());
+            distancia = vluz * tiempo;
+            Console.WriteLine("La distancia recorrida en dicho tiempo es de: " + distancia);
+            Console.ReadKey();
+            
+
+            
+        }
+    }
+}
